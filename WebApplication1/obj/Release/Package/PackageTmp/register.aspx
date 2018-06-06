ae769216a317d9c8f3ab908fc2660cbd04cf9bbd
Michael <michael.ohara@hotmail.co.nz>
2018-06-06 18:32:29 +1200

    first

---
+++ WebApplication1/obj/Release/Package/PackageTmp/register.aspx
@@ -0,0 +1,101 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="register.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" type="text/css" href="mystyle.css" />
+    <title>Home | Fuchsia Register</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="pass">
+            <asp:Label ID="userLabel" runat="server" Text="Beardie"></asp:Label>
+            <asp:Label ID="passLabel" runat="server" Text="Oliver"></asp:Label>
+        </div>
+        <div id="literal">
+        <asp:Literal ID="Literal1" runat="server" Text="
+            &lt;div class=&quot;helpLabel&quot; id=&quot;leftLabel&quot;&gt;&lt;img src=&quot;images/qMark.png&quot; /&gt;
+                Change the size of the text displayed in the table.
+                Log in as Admin to change the data file and upload more images.
+                To disable these tips, click 'Help!' again.
+            &lt;/div&gt;
+            &lt;div class=&quot;helpLabel&quot; id=&quot;centerLabel&quot;&gt;&lt;img src=&quot;images/qMark.png&quot; /&gt;
+                Click the 'Select' button beside any of the flowers in the table to see images.
+                If one or more images are available, the corresponding buttons will become clickable.
+            &lt;/div&gt;
+            &lt;div class=&quot;helpLabel&quot; id=&quot;centerLabel2&quot;&gt;&lt;img src=&quot;images/qMark.png&quot; /&gt;
+                The green box will show the flower you have selected. The search button will take you to
+                a google image search for 'fuchsia' and the name of the selected flower.
+            &lt;/div&gt;
+            &lt;div class=&quot;helpLabel&quot; id=&quot;tableLabel&quot;&gt;&lt;img src=&quot;images/qMark.png&quot; /&gt;
+                To search the register, press ctrl & F on your keyboard to open the 'Find' toolbar.
+                With this you can search for text on the page; note that it will only return exact matches.
+            &lt;/div&gt;
+
+        " Visible="False"></asp:Literal>
+            </div>
+        <div id="header">
+            <div id="left">
+                <asp:DropDownList ID="DropDownList1" runat="server" 
+                OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True">
+                <asp:ListItem>change text size...</asp:ListItem>
+                <asp:ListItem>Small</asp:ListItem>
+                <asp:ListItem>Medium (Default)</asp:ListItem>
+                <asp:ListItem>Large</asp:ListItem>
+                <asp:ListItem>Monstrous</asp:ListItem>
+                </asp:DropDownList>
+                <asp:Button ID="Button7" runat="server" Text="Help!" OnClick="Button7_Click" CssClass="cursor" />
+            </div>
+            <div id="nav">
+            <table>
+                <tr>
+                    <td id="errorBox" colspan="2">
+                        <asp:Label ID="Label1" runat="server"></asp:Label>
+                    </td>
+                    <td id="selectedBox">
+                        <asp:Label ID="Label2" runat="server" Text="selected flower"></asp:Label>
+                    </td>
+                    <td class="imageButton">
+                        <asp:Button ID="Button3" runat="server" OnClick="search_Click" 
+                            Text="Search Online" OnClientClick="target='_blank'" CssClass="cursor"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="imageButton">
+                        <asp:Button ID="Button1" runat="server" OnClick="image1_Click" Text=" " OnClientClick="target='_blank'" Enabled="False"/>
+                    </td>
+                    <td class="imageButton">
+                        <asp:Button ID="Button4" runat="server" OnClick="image2_Click" Text=" " OnClientClick="target='_blank'" Enabled="False"/>
+                    </td>
+                    <td class="imageButton">
+                        <asp:Button ID="Button5" runat="server" OnClick="image3_Click" Text=" " OnClientClick="target='_blank'" Enabled="False"/>
+                    </td>
+                    <td class="imageButton">
+                        <asp:Button ID="Button6" runat="server" OnClick="image4_Click" Text=" " OnClientClick="target='_blank'" Enabled="False"/>
+                    </td>
+                </tr>
+            </table>
+            </div>
+        </div>
+        <div id="admin">  
+            <div id="p">Admin</div>
+            
+                <asp:TextBox ID="userTextBox" runat="server">Admin Username</asp:TextBox>
+                <asp:TextBox ID="passTextBox" runat="server">Admin Password</asp:TextBox>
+                <asp:Button CssClass="cursor" ID="loginButton" runat="server" Text="Log In" OnClick="login_Click" />
+                <asp:Button CssClass="cursor" ID="logoutButton" runat="server" Text="Log Out" OnClick="logout_Click" />
+                <asp:FileUpload ID="upload" runat="server" Enabled="false"/>
+                <asp:Button ID="imageUpload" runat="server" Enabled="false" OnClick="upload_Click" Text="Upload Image" />
+                <asp:Button ID="csvUpload" runat="server" Enabled="false" Text="Overwrite Register" OnClick="csvUpload_Click" />
+            
+        </div>
+        <div id="main">
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateSelectButton="True" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" Font-Size="Medium" Font-Bold="True">
+            <SelectedRowStyle CssClass="selectedRow" Font-Bold="false" BackColor="#92C75C" BorderColor="Black" ForeColor="White" />
+        </asp:GridView>
+    </div>
+        
+    </form>
+</body>
+</html>
---
+++ WebApplication1/register.aspx.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Label1.Text == "")
+            {
+                string path = Server.MapPath("images/fuchsiaRegister.csv");
+                if (File.Exists(path))
+                {
+                    string[] data = File.ReadAllLines(path);
+
+                    DataTable dt = new DataTable();
+
+                    string[] col = data[0].Split(',');
+
+                    foreach (string s in col)
+                    {
+                        dt.Columns.Add(s, typeof(string));
+                    }
+
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        string[] row = data[i].Split(',');
+                        dt.Rows.Add(row);
+                    }
+
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                    Label1.Text = "'Select' a flower to view images";
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int size = DropDownList1.SelectedIndex;
+            switch (size)
+            {
+                case 1:
+                    GridView1.Font.Size = 10;
+                    break;
+                case 2:
+                    GridView1.Font.Size = 12;
+                    break;
+                case 3:
+                    GridView1.Font.Size = 15;
+                    break;
+                case 4:
+                    GridView1.Font.Size = 18;
+                    break;
+            }
+        }
+
+        protected void search_Click(object sender, EventArgs e)
+        {
+
+            GridViewRow selectedRow = GridView1.SelectedRow;
+            if (selectedRow != null)
+            {
+                string rowName = selectedRow.Cells[1].Text;
+                Response.Redirect("https://www.google.com/search?site=imghp&tbm=isch&source=hp&biw=1304&bih=673&q=fuchsia+" + rowName + "&oq=fuchsia+" + rowName + "&gs_l=img.3...169095.173090.1.173223.15.5.1.9.0.0.277.515.2-2.2.0....0...1ac.1.64.img..17.5.813.p6IplvsSwrk");
+
+            }
+            else
+                Label1.Text = "'Select' a flower to search for";
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Button1.Enabled = false;
+            Button4.Enabled = false;
+            Button5.Enabled = false;
+            Button6.Enabled = false;
+            Button1.Text = "";
+            Button4.Text = "";
+            Button5.Text = "";
+            Button6.Text = "";
+            Button1.CssClass = "";
+            Button4.CssClass = "";
+            Button5.CssClass = "";
+            Button6.CssClass = "";
+            GridViewRow selectedRow = GridView1.SelectedRow;
+            Label2.Text = selectedRow.Cells[1].Text;
+            string imagePath = "images/" + selectedRow.Cells[1].Text;
+            string image = imagePath + ".jpg";
+            string image2 = imagePath + " 2.jpg";
+            string image3 = imagePath + " 3.jpg";
+            string image4 = imagePath + " 4.jpg";
+            
+            string path = Server.MapPath(image);
+            string path2 = Server.MapPath(image2);
+            string path3 = Server.MapPath(image3);
+            string path4 = Server.MapPath(image4);
+
+            if (File.Exists(path) && File.Exists(path2) && File.Exists(path3) && File.Exists(path4))
+            {
+                Button1.Enabled = true;
+                Button4.Enabled = true;
+                Button5.Enabled = true;
+                Button6.Enabled = true;
+                Button1.Text = "Image 1";
+                Button4.Text = "Image 2";
+                Button5.Text = "Image 3";
+                Button6.Text = "Image 4";
+                Button1.CssClass = "cursor";
+                Button4.CssClass = "cursor";
+                Button5.CssClass = "cursor";
+                Button6.CssClass = "cursor";
+                Label1.Text = "Click the buttons below to see images";
+            }
+            else if (File.Exists(path) && File.Exists(path2) && File.Exists(path3))
+            {
+                Button1.Enabled = true;
+                Button4.Enabled = true;
+                Button5.Enabled = true;
+                Button1.Text = "Image 1";
+                Button4.Text = "Image 2";
+                Button5.Text = "Image 3";
+                Button1.CssClass = "cursor";
+                Button4.CssClass = "cursor";
+                Button5.CssClass = "cursor";
+                Label1.Text = "Click the buttons below to see images";
+            }
+            else if (File.Exists(path) && File.Exists(path2))
+            {
+                Button1.Enabled = true;
+                Button4.Enabled = true;
+                Button1.Text = "Image 1";
+                Button4.Text = "Image 2";
+                Button1.CssClass = "cursor";
+                Button4.CssClass = "cursor";
+                Label1.Text = "Click the buttons below to see images";
+            }
+            else if (File.Exists(path))
+            {
+                Button1.Enabled = true;
+                Button1.Text = "Image 1";
+                Button1.CssClass = "cursor";
+                Label1.Text = "Click the button below to see an image";
+            }
+            else
+            {
+                Label1.Text = "There are no images available for that flower";
+                
+            }
+        }
+
+        protected void image1_Click(object sender, EventArgs e)
+        {
+            GridViewRow selectedRow = GridView1.SelectedRow;
+            string image = "images/" + selectedRow.Cells[1].Text + ".jpg";
+            Response.Redirect(image);
+        }
+
+        protected void image2_Click(object sender, EventArgs e)
+        {
+            GridViewRow selectedRow = GridView1.SelectedRow;
+            string image = "images/" + selectedRow.Cells[1].Text + " 2.jpg";
+            Response.Redirect(image);
+        }
+        
+        protected void image3_Click(object sender, EventArgs e)
+        {
+            GridViewRow selectedRow = GridView1.SelectedRow;
+            string image = "images/" + selectedRow.Cells[1].Text + " 3.jpg";
+            Response.Redirect(image);
+        }
+
+        protected void image4_Click(object sender, EventArgs e)
+        {
+            GridViewRow selectedRow = GridView1.SelectedRow;
+            string image = "images/" + selectedRow.Cells[1].Text + " 4.jpg";
+            Response.Redirect(image);
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            if (Literal1.Visible == true)
+                Literal1.Visible = false;
+            else 
+                Literal1.Visible = true;
+        }
+
+        protected void login_Click(object sender, EventArgs e)
+        {
+            if (userTextBox.Text == userLabel.Text && passTextBox.Text == passLabel.Text)
+            {
+                upload.Enabled = true;
+                imageUpload.Enabled = true;
+                imageUpload.CssClass = "cursor";
+                csvUpload.Enabled = true;
+                csvUpload.CssClass = "cursor";
+                Label1.Text = "Logged in successfully!";
+            }
+            else
+            {
+                Label1.Text = "The Admin details entered were incorrect";
+            }
+        }
+
+        protected void logout_Click(object sender, EventArgs e)
+        {
+            upload.Enabled = false;
+            imageUpload.Enabled = false;
+            imageUpload.CssClass = "";
+            csvUpload.Enabled = false;
+            csvUpload.CssClass = "";
+            Label1.Text = "Thanks " + userLabel.Text + ", you have been logged out";
+        }
+
+        protected void upload_Click(object sender, EventArgs e)
+        {
+            if (upload.HasFile == false)
+            {
+                Label1.Text = "Please first select a file to upload...";
+            }
+            else
+            {
+                GridViewRow selectedRow = GridView1.SelectedRow;
+                if (selectedRow == null)
+                {
+                    Label1.Text = "Select a flower to associate with your image";
+                }
+                else
+                {
+                    string imagePath = "~/images/" + selectedRow.Cells[1].Text;
+                    string path = Server.MapPath(imagePath + ".jpg");
+                    string path2 = Server.MapPath(imagePath + " 2.jpg");
+                    string path3 = Server.MapPath(imagePath + " 3.jpg");
+                    string path4 = Server.MapPath(imagePath + " 4.jpg");
+
+                    if (File.Exists(path) && File.Exists(path2) && File.Exists(path3) && File.Exists(path4))
+                    {
+                        Label1.Text = "Maximum images (4) reached for that flower";
+                    }
+                    else if (File.Exists(path) && File.Exists(path2) && File.Exists(path3))
+                    {
+                        upload.SaveAs(path4);
+                    }
+                    else if (File.Exists(path) && File.Exists(path2))
+                    {
+                        upload.SaveAs(path3);
+                    }
+                    else if (File.Exists(path))
+                    {
+                        upload.SaveAs(path2);
+                    }
+                    else
+                    {
+                        upload.SaveAs(path);
+                    }
+                    GridView1_SelectedIndexChanged(this, e);
+                }
+            }
+        }
+
+        protected void csvUpload_Click(object sender, EventArgs e)
+        {
+            if (upload.HasFile == false)
+            {
+                Label1.Text = "Please first select a file to upload...";
+            }
+            else
+            {
+                string fileName = upload.PostedFile.FileName;
+                string extension = fileName.Substring(fileName.Length - 4);
+                if (extension == ".csv")
+                {
+                    string filePath = Server.MapPath("~/images/fuchsiaRegister.csv");
+                    upload.SaveAs(filePath);
+
+                    Label1.Text = "";
+                    Page_Load(this, e);
+                }
+                else
+                    Label1.Text = "The uploaded file must be in .csv format";
+            }
+        }
+    }       
+}
